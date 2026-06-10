af548ec2c30a8928e3dd1edd80e765fc7bebfde0
luiz62730 <luiz60581556@alunos.sc.senac.br>
2026-06-10 15:34:51 -0300

    ç

--- THECAGE/Assets/Script/mov.cs
+++ THECAGE/Assets/Script/mov.cs
@@ -32,6 +32,19 @@ public class mov : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
+
+        float InputAxis = Input.GetAxis("Horizontal");
+
+        if (InputAxis > 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 0f);
+        }
+
+        else if (InputAxis < 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 180f);
+        }
+
     }
 
     void Jump()
