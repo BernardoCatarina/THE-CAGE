d02f8cbc56003fc07036cacf2dd8523a8b11c5a7
luiz62730 <luiz60581556@alunos.sc.senac.br>
2026-07-02 10:33:08 -0300

    erro da faca feito

--- THECAGE/Assets/Script/faca.cs
+++ THECAGE/Assets/Script/faca.cs
@@ -7,7 +7,7 @@ public class faca : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GetComponent<Rigidbody2D>().linearVelocity = new Vector2(speed, 0);
+        GetComponent<Rigidbody2D>().linearVelocity = transform.right * speed;
     }
 
     // Update is called once per frame
