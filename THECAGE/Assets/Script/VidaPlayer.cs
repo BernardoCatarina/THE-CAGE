af265eb9eaa72443e55b9b03bcd9b8c2b464832a
luiz62730 <luiz60581556@alunos.sc.senac.br>
2026-08-24 16:38:37 -0300

    .

--- THECAGE/Assets/Script/VidaPlayer.cs
+++ THECAGE/Assets/Script/VidaPlayer.cs
@@ -1,20 +1,28 @@
+using System.Collections; // <-- OBRIGATÓRIO PARA A COROUTINE (PISCAR) FUNCIONAR
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI; // <-- Mantemos essa linha, ela cuida do Slider e do Text Legacy!
+using UnityEngine.UI;
 
-public class VidaPlayer : MonoBehaviour
+public class VidaPlayer : MonoBehaviour 
 {
     public int vidaMaxima = 3;
     private int vidaAtual;
 
     public Slider barraDeVida;
-    public Text textoDeVida; // <-- MUDOU AQUI: Agora é apenas "Text" (Legacy)
+    public Text textoDeVida;
+
+    // --- VARIÁVEIS NOVAS PARA O PISCAR ---
+    private SpriteRenderer spriteRenderer;
+    public int quantidadeDePiscadas = 3; // Quantas vezes ele vai piscar
+    public float tempoPiscada = 0.1f;    // A velocidade da piscada
 
     void Start()
     {
         vidaAtual = vidaMaxima;
 
-        // Configura a barra
+        // Pega o componente que desenha o player na tela
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
         if (barraDeVida != null)
         {
             barraDeVida.maxValue = vidaMaxima;
@@ -28,13 +36,18 @@ public class VidaPlayer : MonoBehaviour
     {
         vidaAtual -= quantidade;
 
-        // Atualiza a barra visual
         if (barraDeVida != null)
         {
             barraDeVida.value = vidaAtual;
         }
 
         AtualizarTexto();
+
+        // --- COMEÇA O EFEITO VISUAL ---
+        if (spriteRenderer != null)
+        {
+            StartCoroutine(EfeitoPiscar());
+        }
 
         Debug.Log("Ai! O Player tomou dano. Vida restante: " + vidaAtual);
 
@@ -44,7 +57,20 @@ public class VidaPlayer : MonoBehaviour
         }
     }
 
-    // Essa função cuida só de escrever o número na tela
+    // --- NOVA FUNÇÃO: O TEMPORIZADOR DE PISCAR ---
+    IEnumerator EfeitoPiscar()
+    {
+        // Vai repetir esse bloco de acordo com a quantidade de piscadas
+        for (int i = 0; i < quantidadeDePiscadas; i++)
+        {
+            spriteRenderer.enabled = false; // Fica invisível
+            yield return new WaitForSeconds(tempoPiscada); // Espera uma fração de segundo
+
+            spriteRenderer.enabled = true; // Volta a ficar visível
+            yield return new WaitForSeconds(tempoPiscada); // Espera de novo
+        }
+    }
+
     void AtualizarTexto()
     {
         if (textoDeVida != null)
