e9cbf847f95554d23ac30eb8d458384e84714a3b
luiz62730 <luiz60581556@alunos.sc.senac.br>
2026-09-22 13:52:31 -0300

    .

--- THECAGE/Assets/Script/GerenciadorPausa.cs
+++ THECAGE/Assets/Script/GerenciadorPausa.cs
@@ -1,18 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio; 
 
 public class GerenciadorPausa : MonoBehaviour
 {
     public GameObject painelOpcoes;
-    public Slider sliderVolume;
+
+    [Header("Configurações de Áudio Integradas")]
+    public AudioMixer audioMixer; 
+    public Slider sliderMusica;
+    public Slider sliderEfeitos;
 
     void Start()
     {
-        if (sliderVolume != null)
-        {
-            sliderVolume.value = AudioListener.volume;
-        }
+        
+        float volumeMusicaSalvo = PlayerPrefs.GetFloat("VolMusica", -10f);
+        float volumeEfeitosSalvo = PlayerPrefs.GetFloat("VolEfeitos", -10f);
+
+        if (sliderMusica != null) sliderMusica.value = volumeMusicaSalvo;
+        if (sliderEfeitos != null) sliderEfeitos.value = volumeEfeitosSalvo;
+
+
+        MudarVolumeMusica(volumeMusicaSalvo);
+        MudarVolumeEfeitos(volumeEfeitosSalvo);
     }
 
     public void AbrirOpcoes()
@@ -29,26 +40,35 @@ public class GerenciadorPausa : MonoBehaviour
 
     public void VoltarParaMenu()
     {
-       
         Time.timeScale = 1f;
 
-       
         ControleTransicao transicao = FindObjectOfType<ControleTransicao>();
-
         if (transicao != null)
         {
-            
             transicao.IrParaProximaFase("menu");
         }
         else
         {
-            
             SceneManager.LoadScene("menu");
         }
     }
 
-    public void AlterarVolume(float valor)
+    // ---------------- FUNÇÕES DO VOLUME (IDÊNTICAS AS DO MENU) ----------------
+    public void MudarVolumeMusica(float volume)
     {
-        AudioListener.volume = valor;
+        if (audioMixer != null)
+        {
+            audioMixer.SetFloat("VolMusica", volume);
+            PlayerPrefs.SetFloat("VolMusica", volume); 
+        }
+    }
+
+    public void MudarVolumeEfeitos(float volume)
+    {
+        if (audioMixer != null)
+        {
+            audioMixer.SetFloat("VolEfeitos", volume);
+            PlayerPrefs.SetFloat("VolEfeitos", volume); 
+        }
     }
 }
